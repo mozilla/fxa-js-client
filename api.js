462cb213935bdb0bda7fed37d4672f049b67479e
vladikoff <vlad.filippov@gmail.com>
2014-01-09 21:21:37 -0500

    Built fxa-js-client from commit 0501867 on branch master

---
+++ api.js
@@ -0,0 +1,12 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "FxAccountClient",
+        "credentials",
+        "hkdf",
+        "pbkdf2"
+    ],
+    "modules": [],
+    "allModules": []
+} };
+});
